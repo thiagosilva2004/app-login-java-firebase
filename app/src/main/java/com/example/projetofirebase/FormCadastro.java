1d6fac350594b8593eb03020a5717ff2ad3a108a
thiagosilva2004 <playerthiago3@gmail.com>
2022-08-14 15:10:42 -0300

    frist commit

---
+++ app/src/main/java/com/example/projetofirebase/FormCadastro.java
@@ -0,0 +1,156 @@
+package com.example.projetofirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Document;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class FormCadastro extends AppCompatActivity {
+
+    private EditText editNome, editEmail, editSenha;
+    private Button btnCadastrar;
+    String[] mensagens = {"preencha o campo nome",
+                          "preencha o campo email",
+                          "preencha o campo senha",
+                          "cadastro realizado com sucesso"};
+    String usuarioId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+
+        getSupportActionBar().hide();
+        inicializar_componetes();
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nome = editNome.getText().toString();
+                String email = editEmail.getText().toString();
+                String senha = editSenha.getText().toString();
+
+                if (nome.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+                else if (email.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(view, mensagens[1], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+                else if (senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(view, mensagens[2], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+                else{
+                    CadastrarUsuario(view);
+                }
+
+            }
+        });
+    }
+
+    private void CadastrarUsuario(View v){
+        String email = editEmail.getText().toString();
+        String senha = editSenha.getText().toString();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword
+                (email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    salvarDadosUsuario();
+
+                    Snackbar snackbar = Snackbar.make(v, mensagens[3], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                    String erro;
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e) {
+                        erro = "Digite uma senha com no mínimo 6 caracteres";
+                    }catch (FirebaseAuthUserCollisionException e){
+                        erro = "Email já cadastrado";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        erro = "Email inválido";
+                    }
+                    catch (Exception e){
+                        erro = "Erro ao cadastrar o usuario";
+                    }
+
+                    Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }
+            }
+        });
+    }
+
+    private void salvarDadosUsuario(){
+        String nome = editNome.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> usuarios = new HashMap<>();
+        usuarios.put("nome", nome);
+
+        usuarioId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioId);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void avoid) {
+                Log.d("db", "Sucesso ao salvar os dados");
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("db_error", "Erro ao salvar os dados" + e.toString());
+            }
+        });
+
+    }
+
+    private void inicializar_componetes(){
+        editNome = findViewById(R.id.edit_nome);
+        editEmail = findViewById(R.id.edit_email);
+        editSenha = findViewById(R.id.edit_senha);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+    }
+}
